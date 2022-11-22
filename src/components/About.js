583b7f82bd398f68445b4c0df1cc468bd2663c7a
Anna Harder <annaqharder@gmail.com>
2022-11-22 09:36:55 -0700

    commit tree 83cf349cc5a5f2f522aaec85eec6bf76e602e9f3

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
+
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="about">About
+      <h2>About Me</h2>
+      <p>Hello, my name is Liza & welcome to my website!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
